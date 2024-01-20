02866620d087fa87d45e2ae4566627d8ce037a14
Chibueze Thankgod <117307528+Buezeth@users.noreply.github.com>
2024-01-20 14:09:27 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const btn = document.querySelector('.button-link');
 btn.addEventListener("mouseover", (event) => {
     const x = event.pageX - btn.offsetLeft;
     const y = event.pageY - btn.offsetTop;
+
+    console.log(event.pageX )
     
-    console.log(x);
-    console.log(y);
+    // console.log(x);
+    // console.log(y);
     btn.style.setProperty("--xPos", y + "px");
     btn.style.setProperty("--yPos", x + "px");
 });
